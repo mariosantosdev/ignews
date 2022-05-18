cbc504b9afa608e7284c3465a3f15752e1aea53b
Mário Santos <mariodev7@gmail.com>
2022-05-18 13:23:17 -0300

    insert user in faunadb

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { query as q } from "faunadb";
+
+import { fauna } from "@services/fauna";
 
 export default NextAuth({
   secret: process.env.NEXTAUTH_SECRET,
@@ -11,4 +14,28 @@ export default NextAuth({
         "https://github.com/login/oauth/authorize?scope=read:user+user:email",
     }),
   ],
+  callbacks: {
+    async signIn({ user }) {
+      try {
+        const { email } = user;
+
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("index_by_email"), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("index_by_email"), q.Casefold(user.email)))
+          )
+        );
+
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+  },
 });

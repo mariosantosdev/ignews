62c52289badc6849747b57688c346460e167ec9e
Mário Santos <mariodev7@gmail.com>
2022-05-18 01:55:24 -0300

    create header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import styles from "./styles.module.scss";
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Image
+          src="/images/logo.svg"
+          alt="Logo ig.news"
+          width={110}
+          height={31}
+        />
+
+        <nav>
+          <Link href="/">
+            <a className={styles.active}>Home</a>
+          </Link>
+          <Link href="/posts">
+            <a>Postagens</a>
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
+import { Header } from "@components/Header";
 import "@styles/global.scss";
 
 import { AppProps } from "next/app";
+import { Fragment } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Fragment>
+      <Header />
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
 
 export default MyApp;

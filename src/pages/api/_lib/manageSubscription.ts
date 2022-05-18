5b490babe2d7ae7ce616ec41e38d396e4082744a
Mário Santos <mariodev7@gmail.com>
2022-05-18 15:36:51 -0300

    update subscription status on database

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -5,7 +5,8 @@ import { stripe } from "@services/stripe";
 
 export async function saveSubscription(
   subscriptionId: string,
-  customerId: string
+  customerId: string,
+  createAction: boolean = false
 ) {
   const userRef = await fauna.query(
     q.Select("ref", q.Get(q.Match(q.Index("index_by_customer_id"), customerId)))
@@ -20,7 +21,19 @@ export async function saveSubscription(
     priceId: subcription.items.data[0].price.id,
   };
 
-  await fauna.query(
-    q.Create(q.Collection("subscriptions"), { data: subcriptionData })
-  );
+  if (createAction) {
+    await fauna.query(
+      q.Create(q.Collection("subscriptions"), { data: subcriptionData })
+    );
+  } else {
+    await fauna.query(
+      q.Replace(
+        q.Select(
+          "ref",
+          q.Get(q.Match(q.Index("subscriptions_by_id"), subscriptionId))
+        ),
+        { data: subcriptionData }
+      )
+    );
+  }
 }
--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -21,7 +21,12 @@ export const config = {
   },
 };
 
-const relevantsEvents = new Set(["checkout.session.completed"]);
+const relevantsEvents = new Set([
+  "checkout.session.completed",
+  "customer.subscription.created",
+  "customer.subscription.updated",
+  "customer.subscription.deleted",
+]);
 
 export default async function Webhooks(
   req: NextApiRequest,
@@ -55,6 +60,17 @@ export default async function Webhooks(
             await saveSubscription(
               checkoutSession.subscription.toString(),
               checkoutSession.customer.toString()
+            );
+            break;
+
+          case "customer.subscription.updated":
+          case "customer.subscription.deleted":
+            const subscription = event.data.object as Stripe.Subscription;
+
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              false
             );
             break;
 

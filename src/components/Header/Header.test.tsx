42046555eb15241af1af25d5c35dc0e0453b3f41
Mário Santos <mariodev7@gmail.com>
2022-06-19 23:33:25 -0300

    create test to header component

---
+++ src/components/Header/Header.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+
+import { Header } from ".";
+
+jest.mock("next/router", () => ({
+  useRouter: () => ({
+    asPath: "/",
+  }),
+}));
+
+jest.mock("next-auth/react", () => ({
+  useSession: () => [null, false],
+}));
+
+describe("Header Component", () => {
+  it("Should render correctly", () => {
+    render(<Header />);
+
+    expect(screen.getByText("Home")).toBeInTheDocument();
+    expect(screen.getByText("Postagens")).toBeInTheDocument();
+  });
+});

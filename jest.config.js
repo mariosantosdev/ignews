3482699b6c98a078f1b64c52782054f4bd08aaaf
Mário Santos <mariodev7@gmail.com>
2022-06-19 23:15:31 -0300

    fix setup jest

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,18 @@
-module.exports = {
-  testingIgnorePattern: ["node_modules", "/.next/"],
-  setupFilesAfterEnv: ["<rootDir>/src/tests/setupTests.ts"],
-  tranforms: {
-    "^.+\\(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+  testPathIgnorePatterns: ["node_modules", "/.next/"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+  },
+  moduleNameMapper: {
+    // Handle module aliases (this will be automatically configured for you soon)
+    "^@/styles/(.*)$": "<rootDir>/src/styles/$1",
+    "^@/components/(.*)$": "<rootDir>/src/components/$1",
+    "^@/services/(.*)$": "<rootDir>/src/services/$1",
   },
   testEnvironment: "jsdom",
 };
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = customJestConfig;

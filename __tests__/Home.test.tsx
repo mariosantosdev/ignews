5fd02dfeddaa0df5be12b34ddb8d4e65f0f0df6d
Mário Santos <mariodev7@gmail.com>
2022-06-27 23:46:24 -0300

    feature: create test  of getStaticProps

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,11 +1,14 @@
 import { render, screen } from "@testing-library/react";
+import { mocked } from "jest-mock";
 
-import Home from "@pages/index";
+import Home, { getStaticProps } from "@pages/index";
+import { stripe } from "@services/stripe";
 
 jest.mock("next/router");
 jest.mock("next-auth/react", () => ({
   useSession: () => [null, false],
 }));
+jest.mock("../src/services/stripe");
 
 describe("Home Page", () => {
   it("renders correctly", () => {
@@ -19,5 +22,27 @@ describe("Home Page", () => {
     );
 
     expect(screen.getByText("por apenas R$10,00 mensais.")).toBeInTheDocument();
+  });
+
+  it("loading initial data", async () => {
+    const retrieveStripePricesMocked = mocked(stripe.prices.retrieve);
+
+    retrieveStripePricesMocked.mockResolvedValueOnce({
+      id: "1",
+      unit_amount: 1000,
+    } as any);
+
+    const response = await getStaticProps({});
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          product: {
+            amount: expect.stringContaining("10,00"),
+            priceId: "1",
+          },
+        },
+      })
+    );
   });
 });
